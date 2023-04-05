3d519a7e237acd156caaff5b12ef34c1dd79fa94
Halim Choi <hchoicareer@gmail.com>
2023-04-05 15:21:46 -0400

    added recipe instructions and toggle button for selected recipe

--- script.js
+++ script.js
@@ -50,18 +50,48 @@ function displaySelectedRecipe(recipe) {
     const recipeId = recipe.id;
     const recipeTitle = document.createElement("h3");
     const recipeImage = document.createElement("img");
+    const instructionBtn = document.createElement("button");
     recipeImage.src = recipe.image;
     recipeTitle.textContent = recipe.title;
+    instructionBtn.textContent = "Show Instructions";
+    instructionBtn.id = "instruction-btn"
     selectedArea.appendChild(recipeTitle);
     selectedArea.appendChild(recipeImage);
-    getInstructions(recipeId);
+    selectedArea.appendChild(instructionBtn);
+    listenInstructionBtn(recipeId);
 }
 
 function getInstructions(id) {
     const baseUrl = `https://api.spoonacular.com/recipes/${id}/analyzedInstructions?query=&apiKey=${apiKey}`;
     fetch(baseUrl)
         .then(res => res.json())
-        .then(instructions => console.log(instructions))
+        .then(instructionData => displayInstructions(instructionData))
+}
+
+function displayInstructions(instructionData) {
+    console.log(instructionData[0].steps);
+    const instructionSection = document.getElementById("selected-recipe-instructions");
+    instructionSection.innerHTML = "";
+    instructionData[0].steps.forEach(step => {
+        let instructionMsg = document.createElement("li")
+        instructionMsg.textContent = `${step.number}. ${step.step}`
+        instructionSection.appendChild(instructionMsg)
+    });
+}
+
+function listenInstructionBtn(id) {
+    const button = document.getElementById("instruction-btn");
+    button.addEventListener("click", () => {
+        getInstructions(id)
+        const instructions = document.getElementById("selected-recipe-instructions");
+        if (instructions.style.display === "block") {
+            instructions.style.display = "none";
+            button.textContent = "Show Instructions"
+        } else {
+            instructions.style.display = "block";
+            button.textContent = "Hide Instructions"
+        }
+    });
 }
 
 // function getSelectedRecipe(e) {
@@ -71,21 +101,3 @@ function getInstructions(id) {
 // }
 
 listenSubmit();
-
-
-
-//const typeInput = document.getElementById("mealtype").value;
-//     let url = `${requestURL}${searchInput}&apiKey=${apiKey}`;
-//     if (cuisineInput != "" && typeInput == "") {
-//         url = `${requestURL}${searchInput}&cuisine=${cuisineInput}&apiKey=${apiKey}`;
-//     } else if (typeInput != "" && cuisineInput == "") {
-//         url = `${requestURL}${searchInput}&type=${typeInput}&apiKey=${apiKey}`;
-//     } else if (typeInput != "" && cuisineInput != "") {
-//         url = `${requestURL}${searchInput}&type=${typeInput}&cuisine=${cuisineInput}&apiKey=${apiKey}`;
-//     }
-//     console.log(searchInput);
-
-//     fetch(url)
-//         .then(res => res.json())
-//         .then(recipes => displayRecipes(recipes))
-//     e.target.reset();
