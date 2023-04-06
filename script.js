25ef4812022148da97b4dce94f91113b9ead41b4
JoshuaFowler <jfowler8912@gmail.com>
2023-04-06 16:38:54 -0400

    this branch to be unused

--- script.js
+++ script.js
@@ -1,18 +1,50 @@
-const requestURL = "https://api.spoonacular.com/recipes/findByIngredients?ingredients=";
+// const requestURL = "https://api.spoonacular.com/recipes/findByIngredients?ingredients=";
+const requestURL = "https://api.spoonacular.com/recipes/complexSearch?query=";
 
 function listenSubmit() {
     const form = document.getElementById("search-form");
-    form.addEventListener("submit", (e) => getData(e));
+    form.addEventListener("submit", (e) => getRecipes(e));
 }
 
-function getData(e) {
+function cleanResults() {
+    const recipeList = document.getElementById("recipe-list");
+    recipeList.innerHTML = "";
+}
+
+function getRecipes(e) {
     e.preventDefault();
-    const input = document.getElementById("search").value;
-    console.log(input);
-    const url = requestURL + input + "&apiKey=" + apiKey;
+    const searchInput = document.getElementById("search").value;
+    const cuisineInput = document.getElementById("cuisine").value;
+    const typeInput = document.getElementById("mealtype").value;
+    let url = `${requestURL}${searchInput}&apiKey=${apiKey}`;
+    if (cuisineInput != "" && typeInput == "") {
+        url = `${requestURL}${searchInput}&cuisine=${cuisineInput}&apiKey=${apiKey}`;
+    } else if (typeInput != "" && cuisineInput == "") {
+        url = `${requestURL}${searchInput}&type=${typeInput}&apiKey=${apiKey}`;
+    } else if (typeInput != "" && cuisineInput != "") {
+        url = `${requestURL}${searchInput}&type=${typeInput}&cuisine=${cuisineInput}&apiKey=${apiKey}`;
+    }
+    console.log(searchInput);
+    
     fetch(url)
-    .then(res => res.json())
-    .then(data => console.log(data))
+        .then(res => res.json())
+        .then(recipes => displayRecipes(recipes))
+    e.target.reset();
+}
+
+function displayRecipes(recipes) {
+    cleanResults();
+    console.log(recipes.results);
+    const recipeList = document.getElementById("recipe-list");
+
+    recipes.results.forEach(recipe => {
+        const recipeTitle = document.createElement("h3");
+        const recipeImage = document.createElement("img");
+        recipeTitle.textContent = recipe.title;
+        recipeImage.src = recipe.image;
+        recipeList.appendChild(recipeTitle);
+        recipeList.appendChild(recipeImage);
+    });
 }
 
 listenSubmit();
